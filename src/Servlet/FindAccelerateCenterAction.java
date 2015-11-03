a062deae9cbd163073f27e3f88395f4023956489
xtayalf <zhangwj0101@gmail.com>
2015-11-03 12:07:09 +0800

    delete comments

--- src/Servlet/FindAccelerateCenterAction.java
+++ src/Servlet/FindAccelerateCenterAction.java
@@ -13,20 +13,7 @@ import Dao.FindAccelerateCenterDAO;
 
 public class FindAccelerateCenterAction extends HttpServlet {
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 * 
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request
-	 *            the request send by the client to the server
-	 * @param response
-	 *            the response send by the server to the client
-	 * @throws ServletException
-	 *             if an error occurred
-	 * @throws IOException
-	 *             if an error occurred
-	 */
+
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
